d90088cbc94b7f7388e6b62921fd45c0cffd4f4b
51135499 <a172839f@gmail.com>
2022-05-14 14:07:20 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,12 @@
+const N76_ADDR = 0x10
+//% color="#AA278D" weight=100
+namespace BitMouse {
+    //% block
+    //% blockid=sensor_readIR block="read IRsensor |%address"
+    //% address.min=1 address.max=4 address.defl=1
+    export function irRead(address: number): number {
+        pins.i2cWriteNumber(N76_ADDR, address + 2, NumberFormat.UInt8LE, false)
+        return pins.i2cReadNumber(16, NumberFormat.UInt16BE, false)
+    }
+
+}
