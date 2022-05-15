37e686b42629736ca781af52978c346d045b9b41
51135499 <a172839f@gmail.com>
2022-05-16 07:34:03 +0800

    更新 main.ts

--- main.ts
+++ main.ts
@@ -8,10 +8,4 @@ namespace BitMouse {
         pins.i2cWriteNumber(N76_ADDR, address + 2, NumberFormat.UInt8LE, false)
         return pins.i2cReadNumber(16, NumberFormat.UInt16BE, false)
     }
-    //% block
-    //% blockid=sensor_readIR1 block="read IRsensor 1"
-    export function irRead1(): number {
-        pins.i2cWriteNumber(N76_ADDR, 3, NumberFormat.UInt8LE, false)
-        return pins.i2cReadNumber(16, NumberFormat.UInt16BE, false)
-    }
 }
