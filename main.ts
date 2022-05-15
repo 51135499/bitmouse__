e97ffef7246171ef0d72674e348d644c2681f946
51135499 <a172839f@gmail.com>
2022-05-16 07:44:31 +0800

    更新 main.ts

--- main.ts
+++ main.ts
@@ -8,4 +8,22 @@ namespace BitMouse {
         pins.i2cWriteNumber(N76_ADDR, address + 2, NumberFormat.UInt8LE, false)
         return pins.i2cReadNumber(16, NumberFormat.UInt16BE, false)
     }
+    
+    //% blockid=motor_motorRun block="motor|%index|PWM value|%PWM"
+    //% PWM.min=-1000 PWM.max=1000
+    export function motorRun(index: number, PWM:number): void {
+        let i2cbuf = pins.createBuffer(3)
+        if (index == 0) {
+            i2cbuf[0] = 0x00
+            i2cbuf[1] = (PWM >> 8)
+            i2cbuf[2] = PWM
+            pins.i2cWriteBuffer(N76_ADDR, i2cbuf)
+        }
+        if (index == 2) {
+            i2cbuf[0] = 0x02
+            i2cbuf[1] = (PWM >> 8)
+            i2cbuf[2] = PWM
+            pins.i2cWriteBuffer(N76_ADDR, i2cbuf)
+        }        
+    }
 }
